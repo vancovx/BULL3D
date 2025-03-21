9d8e2f93f12333692d5825cc165b4819d55847dc
Vanessa Covrig Covrig <vanessacovrig7@gmail.com>
2025-03-21 22:05:59 +0100

    Goals controller and routes setup

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
+
+
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
+
+
+//Desglose de como se hacen las rutas
+/*
+router.get('/', (req, res) => {
+    res.status(200).json({message: 'Get goals'})
+}
+*/
+
+//router.get('/', getGoals)
+//router.post('/', setGoal)
+    
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Hello World')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 8000
+
+
+//Inicializar express
+const app = express()
+
+//Middleware -> Software que actua de intermediario entre componentes
+//Middleware para poder leer los cuerpos de las peticiones
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//Peticiones que hemos declarado
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+//Middleware para tratar los errores, tiene que ir después de las peticiones?
+app.use(errorHandler)
+
+//Random
+app.listen(port, () => console.log(`Server started on port ${port}`))
