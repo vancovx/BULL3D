22bb4ed0102ff3072e66857d7c5470085b641245
Vanessa Covrig Covrig <vanessacovrig7@gmail.com>
2025-05-22 18:06:11 +0200

    DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,18 @@
-const mogoose = require('mongoose')
+const mongoose = require('mongoose');
+const colors = require('colors');  // Si usas colors para mensajes en consola
 
-//Funcion 
+// Función de conexión a MongoDB
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MondoDB Connectese: ${conn.connection.host}`.cyan.underline);
-    }catch (error){
-        console.log(error);
-        process.exit(1)
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
+        console.error('Error al conectar a MongoDB:', error);
+        process.exit(1);
     }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
